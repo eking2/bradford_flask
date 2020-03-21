290733be38db111a46a63cce8f4033605ce68a33
eking2 <eking2@uci.edu>
2020-03-20 21:52:51 -0700

    start

---
+++ app.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_uploads import UploadSet, configure_uploads, patch_request_class
+from wtforms import SubmitField, IntegerField
+from wtforms.validators import NumberRange
+import pandas as pd
+
+from bradford import bradford_calc
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asdfjkasdfja131je2qja9sd0jaas'
+app.config['UPLOADED_CSVS_DEST'] = Path(Path.cwd(), 'tmp')
+app.config['TMP'] = Path(Path.cwd(), 'tmp')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+csvs = UploadSet(name='csvs', extensions=['csv'])
+configure_uploads(app, csvs)
+
+# set max file size
+patch_request_class(app)
+
+class UploadForm(FlaskForm):
+    csv = FileField('csv', validators=[FileRequired(), FileAllowed(['csv'], 'Upload a CSV file')])
+    poly = IntegerField('Polynomial', validators=[NumberRange(1, 5)], default=2)
+    submit = SubmitField('Upload')
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+
+    # delete leftover files 
+    to_delete = Path('tmp').iterdir()
+    for fn in to_delete:
+        fn.unlink()
+
+    form = UploadForm()
+    if form.validate_on_submit():
+
+        # flask-upload will automatically call secure_filename
+        # saving is not necessary, can read directly into df
+        filename = csvs.save(form.csv.data)
+
+        # remove extension, use to name output files
+        fn = filename[:-4]
+        session['fn'] = fn
+
+        # read df and run bradford calc
+        df = pd.read_csv(csvs.path(filename))
+        bradford = bradford_calc(df, fn, form.poly.data)
+        bradford.run_all()
+
+        return redirect(url_for('results'))
+    else:
+
+        return render_template('index.html', form=form)
+
+@app.route('/results')
+def results():
+
+    fn = session.pop('fn', None)
+    
+    if fn:
+        df = pd.read_csv(f'{app.config["TMP"]}/{fn}_concs.csv')
+        return render_template('results.html', fn=fn, df=df.to_html())
+    else:
+        # prevent users from accessing results without submitting
+        return 'No results'
+
+# download the ouput
+@app.route('/tmp/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['TMP'], filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+
+    app.run()
---
+++ bradford.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from sympy import S, symbols, printing
+
+cwd = Path().absolute()
+
+class bradford_calc:
+
+    def __init__(self, df, filename, poly=2):
+
+        '''
+        determine protein concentration from absorbance readings
+
+        df : dataframe
+            absorbances for standards and samples, and dilution factors
+        poly : int
+            polynomial degree to fit data to (default = 2)
+        '''
+
+        self.df = df
+        self.filename = filename
+        self.poly = poly
+
+        self.std = None
+        self.proteins = None
+
+    def _subset_data(self):
+
+        '''
+        split abs into standard curve
+        and sample absorbances
+        '''
+
+        std = self.df.query("Dil == 'std'")
+        proteins = self.df.query("Dil != 'std'")
+
+        # convert str to numeric
+        std = std.assign(Sample = lambda x: pd.to_numeric(x['Sample']))
+
+        return std, proteins
+
+    def _fit_std_curve(self, std):
+
+        '''
+        fit standard curve to polynomial
+        '''
+
+        # fit
+        p = np.poly1d(np.polyfit(std['abs_595'], std['Sample'], self.poly))
+
+        # calc error and correlation in best fit curve to data
+        residuals = std['Sample'] - p(std['abs_595'])
+        ss_res = np.sum(residuals**2)
+        ss_tot = np.sum((std['Sample'] - np.mean(std['Sample']))**2)
+        r_sq = 1 - (ss_res / ss_tot)
+
+        return p, r_sq
+
+    def _plot_bradford(self, std, proteins, p, r_sq):
+
+        '''
+        plot bradford standard curve and sample absorbance data points
+        '''
+
+        # plot
+        fig, ax = plt.subplots(figsize=(6,5))
+
+        # standard curve and labeled points
+        xra = np.linspace(std['abs_595'].min(), std['abs_595'].max(), 100)
+        plt.plot(xra, p(xra), lw=2.5, color='orange', zorder=0)
+        plt.scatter(std['abs_595'], std['Sample'], edgecolor='k', alpha=0.5,
+                    label='BSA standards', marker='s')
+
+        # plot replicates of each sample abs
+        for samp in proteins['Sample'].unique():
+            temp = proteins[proteins['Sample'] == samp]
+            plt.scatter(temp['abs_595'], p(temp['abs_595']), label=samp, edgecolor='k', alpha=0.8)
+
+        plt.grid(alpha=0.2)
+        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize='large')
+        ax.set_axisbelow(True)
+
+        # setup poly equation to pretty print
+        x = symbols("x")
+
+        # extract coefficients and reverse so order is decreasing powers
+        poly = sum(S("{:6.1f}".format(v))*x**i for i, v in enumerate(p.c[::-1]))
+        eq_latex = printing.latex(poly)
+
+        annotation_str = '${}$'.format(eq_latex)
+        annotation_str += '\n'
+        annotation_str += r'$R^2$ = {:.3f}'.format(r_sq)
+
+        plt.text(0.05, 0.8, annotation_str, transform=ax.transAxes, fontsize='large')
+        plt.xlabel('OD595')
+        plt.ylabel('Concentration (ug/uL)')
+        plt.title('Bradford Assay Standard Curve')
+
+        plt.savefig(Path(cwd, 'tmp', f'{self.filename}_std_curve.png'), bbox_inches='tight', dpi=300)
+
+
+    def _calc_concs(self, p, proteins):
+
+        '''
+        calc final protein concentrations
+        '''
+
+        # correct for dilution factor in protein samples
+        # convert to mg/ml
+        proteins = proteins.assign(Dil = lambda x: pd.to_numeric(x['Dil']))
+        proteins['conc_mg_ml'] = p(proteins['abs_595']) * proteins['Dil'] / 1000
+
+        # avg results over all replicates
+        grp = proteins.groupby('Sample')['conc_mg_ml'].agg([np.mean, np.std]).reset_index()
+
+        # get stderror (in percent)
+        # and concentrations after diluting with 20% glycerol for storage
+        grp['stderr'] = grp['std']/grp['mean'] * 100
+        grp = grp.rename(columns={'mean' : 'conc_mg_ml'})
+        grp['after_glycerol_mg_ml'] = grp['conc_mg_ml']*0.6
+
+        # save
+        grp.round(2).to_csv(Path(cwd, 'tmp', f'{self.filename}_concs.csv'), index=False)
+
+        return grp.round(2)
+
+    def run_all(self):
+
+        # split data
+        std, proteins = self._subset_data()
+
+        # fit std curve
+        p, r_sq = self._fit_std_curve(std)
+
+        # plot
+        self._plot_bradford(std, proteins, p, r_sq)
+
+        # output results
+        self._calc_concs(p, proteins)
+
+
+
